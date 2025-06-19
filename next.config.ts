2cddeab257c939d5d5b81dfccf5e3393678e79fe
omkar-murala <omkar.developer1579@gmail.com>
2025-06-19 10:11:24 +0530

    new features

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "images.pexels.com", // ✅ added for Pexels image support
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com", // ✅ added for Cloudinary image support
+      },
     ],
   },
 };
--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,17 +1,18 @@
 'use client';
 
+import Image from "next/image";
 import { Slide } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
 
 const slideImages = [
   {
-    url: "https://images.unsplash.com/photo-1628771065518-0d82f1938462?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3",
+    url: "https://res.cloudinary.com/drw4abclv/image/upload/v1750299163/banner1_fkdtbd.jpg",
   },
   {
-    url: "https://plus.unsplash.com/premium_photo-1681996336639-ad2b31aeac11?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3",
+    url: "https://res.cloudinary.com/drw4abclv/image/upload/v1750299166/banner2_xla34u.png",
   },
   {
-    url: "https://plus.unsplash.com/premium_photo-1681995746040-a5f679a3568a?q=80&w=2106&auto=format&fit=crop&ixlib=rb-4.0.3",
+    url: "https://res.cloudinary.com/drw4abclv/image/upload/v1750299164/banner3_unm484.jpg",
   },
 ];
 
@@ -31,15 +32,24 @@ function SlideShow() {
             <div
               className="
                 w-full
-                h-[450px]
-                sm:h-[400px]
-                md:h-[410px]
-                lg:h-[420px]
-                xl:h-[420px]
-                bg-cover bg-center bg-no-repeat
+                h-[250px]
+                sm:h-[350px]
+                md:h-[400px]
+                lg:h-[460px]
+                xl:h-[480px]
+                relative
+                bg-black
               "
-              style={{ backgroundImage: `url(${slideImage.url})` }}
-            />
+            >
+              <Image
+                src={slideImage.url}
+                alt={`Slide ${index}`}
+                fill
+                priority
+                sizes="100vm"
+                className="w-full h-full"
+              />
+            </div>
           </div>
         ))}
       </Slide>
