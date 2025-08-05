9dc0fd44dd1c6c372aeb77c0c69c982dbbbe29a3
shub12388y <spl.sp999@gmail.com>
2025-08-05 23:41:45 +0530

    fix the slider

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,24 +1,26 @@
 "use client";
-import Image from "next/image";
 import { Slide } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
 
 const slideImages = [
   {
-    url: "https://res.cloudinary.com/drw4abclv/image/upload/v1750299163/banner1_fkdtbd.jpg",
+    url: "./header1.png",
   },
   {
-    url: "https://res.cloudinary.com/drw4abclv/image/upload/v1750299166/banner2_xla34u.png",
+    url: "./header2.jpg",
   },
   {
-    url: "https://res.cloudinary.com/drw4abclv/image/upload/v1750299164/banner3_unm484.jpg",
+    url: "./header3.jpg",
+  },
+  {
+    url: "./header4.jpg",
   },
 ];
 
 function SlideShow() {
   return (
     <div className="w-full relative">
-      <div className="w-full aspect-[16/9] sm:aspect-[16/9] md:aspect-[21/9] lg:aspect-[5/2] overflow-hidden rounded-md">
+      <div className="w-full  overflow-hidden rounded-md">
         <Slide
           duration={3000}
           autoplay
@@ -28,16 +30,12 @@ function SlideShow() {
           arrows={true}
         >
           {slideImages.map((slideImage, index) => (
-            <div key={index} className="w-full h-full">
-              <div className="relative w-full h-[100rem]">
-                <Image
-                  suppressHydrationWarning
-                  width={10000}
-                  height={10000}
+            <div key={index} className="w-full">
+              <div className="relative w-full">
+                <img
+                  className="w-full h-full object-cover"
                   src={slideImage.url}
                   alt={`Slide ${index + 1}`}
-                  className="object-cover"
-                  priority={index === 0}
                 />
               </div>
             </div>
