436d2bd0724c62f5a56783bb614fa530d963ed85
omkar-murala <omkar.developer1579@gmail.com>
2025-08-30 13:49:43 +0530

    testofix

--- src/lib/dynamicComponent.ts
+++ src/lib/dynamicComponent.ts
@@ -1,14 +1,23 @@
+import CustomerReview from "@/components/ui/customerreview";
 import OffersAndSatisfaction from "@/components/ui/offer";
 import OurPromise from "@/components/ui/OurPromise";
 import ProductFaq from "@/components/ui/productfaq";
 import TestimonialCarousel from "@/components/ui/testimonials";
-import CustomerReview from "@/components/ui/customerreview";
 
 
 
 export const dynamicComponent = (key: string) => {
   const componentObject = {
     ejacure: {
+      offer: OffersAndSatisfaction,
+      bannerImg: "/Sexuloon Ejacure web design 2.jpg",
+      successStories: TestimonialCarousel,
+      faq: ProductFaq,
+      customerReview: CustomerReview,
+      featureImg: "/Sexuloon Ejacure web design.jpg",
+      cta: OurPromise,
+    },
+    testofix: {
       offer: OffersAndSatisfaction,
       bannerImg: "/Sexuloon Ejacure web design 2.jpg",
       successStories: TestimonialCarousel,
