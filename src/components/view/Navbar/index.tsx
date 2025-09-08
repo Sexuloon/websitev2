d6e77ea2e19508e1b1a793a4e64d30cdb7aa5eb8
omkar-murala <omkar.developer1579@gmail.com>
2025-09-08 17:24:23 +0530

    consultancy

--- src/components/view/Navbar/index.tsx
+++ src/components/view/Navbar/index.tsx
@@ -136,7 +136,7 @@ function Navbar() {
               All Products
             </Link>
             <Link
-              href="/consultation"
+              href="/consultancy"
               className="w-full text-[18px] font-semibold text-black px-4 py-2 rounded-full hover:bg-black hover:text-white transition-all text-left"
             >
               Consultation
